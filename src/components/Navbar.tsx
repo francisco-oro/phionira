9b879b4fc78181fb64db46f23e98603d7731405d
Francisco Oro <francisco.oro@comunidad.unam.mx>
2023-08-31 08:14:10 -0600

    layout/auth

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className='fixed inset-x-0 top-o bg-white darK:bg-gray:950 z-[10] h-fit border-b border-zinc-300 py-2'>
+        <div className='flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl'>
+            <Link href={"/gallery"} className='items-center hidden gap-2 sm:flex'>
+                <p className='rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>
+                    Phionira
+                </p>
+            </Link>
+            <div className='flex items-center'>
+                <Link href={"/enroll"} className=''>
+                
+                </Link>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
