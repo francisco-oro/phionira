43889ee619d93514632daa94c25f59dd487ee820
Francisco Oro <francisco.oro@comunidad.unam.mx>
2023-08-31 16:13:29 -0600

    remove env file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import SignInButton from './SignInButton'
+import { getAuthSession } from '@/lib/auth'
 
-type Props = {}
+type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+    const session = await getAuthSession(); 
+    console.log(session);
   return (
     <nav className='fixed inset-x-0 top-o bg-white darK:bg-gray:950 z-[10] h-fit border-b border-zinc-300 py-2'>
         <div className='flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl'>
@@ -13,9 +17,13 @@ const Navbar = (props: Props) => {
                 </p>
             </Link>
             <div className='flex items-center'>
-                <Link href={"/enroll"} className=''>
-                
+                <Link href={"/create"} className='mr-3'>
+                    Crear Curso
                 </Link>
+                <Link href={"/settings"} className='mr-3'>
+                    Ajustes
+                </Link>
+                <SignInButton></SignInButton>
             </div>
         </div>
     </nav>
